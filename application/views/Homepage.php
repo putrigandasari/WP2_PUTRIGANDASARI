28afc5d1d5f23c596c14c45b18a1b3c359f0d181
putrigandasari <gandasariputri3@gmail.com>
2023-10-05 21:06:18 +0700

    Latihan Pertemuan 3

---
+++ application/views/Homepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+    <style>
+        body {
+            background-color: #D8B863;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tentang Saya</h1>
+    <p>Hai, perkenalkan nama saya Putri Gandasari. Lahir di Bogor 03 Agustus 2003, tinggal
+        di daerah Ciomas - Bogor. Saat ini saya merupakan seorang mahasiswi semester 3 
+        di Universitas Bina Sarana Informatika. Olahraga yang saya sukai adalah jogging dan work out. 
+        Selain itu saya sangat senang bernyanyi dan mengabadikan moment.</p>
+</body>
+</html>
