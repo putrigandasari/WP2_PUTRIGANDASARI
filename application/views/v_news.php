ce6d666f39dc41a8c52abad28b1738a3deab7384
putrigandasari <gandasariputri3@gmail.com>
2023-10-13 13:46:09 +0700

    Latihan Pertemuan 4 - Jurnal Rental Buku

---
+++ application/views/v_news.php
@@ -0,0 +1,28 @@
+<section>
+        <h1><?php echo $judul ?></h1>
+        <center>
+            <img src="<?php echo base_url() ?>assets/img/asnawi.jpg" alt="">
+        </center>
+        <p align=’justify’>Timnas Indonesia akan menghadapi Brunei Darussalam
+            pada putaran pertama Kualifikasi Piala Dunia 2026 di Stadion Utama Gelora Bung Karno
+            (SUGBK), Kamis, 12 Oktober 2023 malam WIB.</p>
+        <p>Bermain di kandang sendiri, Timnas Merah Putih diharapkan bisa menang 
+            dengan skor telak. Dukungan suporter yang bakal memadati SUGBK, diyakini mampu 
+            mendongkrak semangat juang pemain Timnas Indonesia untuk memetik kemenangan.</p>
+                <ol type=”a”>
+        <h1>Tak Boleh Lengah</h1>
+        <p align=’justify’>Timnas Indonesia berhasrat meraih kemenangan dengan skor besar pada laga melawan Brunei Darussalam. 
+            Namun, pelatih Shin Tae-yong memperingatkan agar seluruh pemain tetap fokus sepanjang laga 
+            dan tidak lengah.</p>
+
+        <p align=’justify’>Garuda Calling</p>
+        <center>
+            <img src="<?php echo base_url() ?>assets/img/ayang.jpg" alt="">
+        </center>
+        <p align=’justify’>"Pelatih kepala Timnas Indonesia Shin Tae-yong, kapten Timnas Indonesia Asnawi Mangkualam, 
+            kapten Timnas Brunei Darussalam Mohammad Hendra Azam dan pelatih kepala Timnas Brunei Darussalam Mario Rivera 
+            saat menghadiri acara konferensi pers di ruang media center Stadion Utama Gelora Bung Karno 
+            (SUGBK) jelang laga putaran pertama Kualifikasi Piala Dunia 2026 antara Indonesia menghadapi 
+            Brunei Darussalam yang akan berlangsung pada Kamis (12/10/2023) malam di SUGBK..</p>
+        <p align=’justify’> "Yang Pasti Kemenangan Harus Bisa Kita Raih" - Asnawi Mangkualam</p>
+</section>
